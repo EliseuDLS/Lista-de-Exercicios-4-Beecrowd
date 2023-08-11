1ad60823ca78b88bd774e92af13e7c3620f8f8a4
Eliseu <124800294+EliseuDLS@users.noreply.github.com>
2023-08-10 23:45:45 -0300

    Update Ex_Beecrowd_1176.java

--- Ex_Beecrowd_1176.java
+++ Ex_Beecrowd_1176.java
@@ -3,50 +3,44 @@ public class Ex_Beecrowd_1176{
     final static Scanner LER = new Scanner(System.in);
     public static void main(String[] args){
 
-        int tamanhoVetor = 5;
+        int tamanhoVetor = 61;
         long fib[] = new long[tamanhoVetor];
 
-        int t = 0;
-        int n = 0;
+        int t = lerT();
+        montarFib(fib);
 
-        t = lerT(t);
-        fib = montarFib(fib);
-
-        for (int i = 0; i < t; i++){
-            n = lerN(n);
+        for (int i = 0; i < t; i++) {
+            int n = lerN();
             imprimirFibN(fib, n);
         }
     }
 
-    public static int lerT(int t){
-
-        t = LER.nextInt();
-
+    public static int lerT() {
+        int t = LER.nextInt();
+        
         return t;
     }
 
-    public static int lerN(int n){
-
+    public static int lerN() {
+        int n = 0;
+        
         do {
             n = LER.nextInt();
-        } while (n > 0 || n < 60);
-
+        } while (n < 0 || n > 60);
+        
         return n;
     }
 
-    public static long[] montarFib(long[] fib){
-
+    public static void montarFib(long[] fib) {
         fib[0] = 0;
         fib[1] = 1;
 
-        for (int i = 2; i < fib.length; i++){
+        for (int i = 2; i < fib.length; i++) {
             fib[i] = fib[i - 2] + fib[i - 1];
         }
-
-        return fib;
     }
 
-    public static void imprimirFibN(long[] fib, int n){
-        System.out.println("Fib[" + n + "] = " + fib[n]);
+    public static void imprimirFibN(long[] fib, int n) {
+        System.out.println("Fib(" + n + ") = " + fib[n]);
     }
 }
